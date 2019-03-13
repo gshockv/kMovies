9c1cffecf2da002a947e99df9afb1c8dde738245
Vitaly Gashock <gshockv@gmail.com>
2019-03-13 16:26:53 +0200

    API key stored in hidden .properties file

--- app/src/main/java/com/gshockv/kmovies/MoviesListFragment.kt
+++ app/src/main/java/com/gshockv/kmovies/MoviesListFragment.kt
@@ -4,14 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 
 class MoviesListFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater.inflate(R.layout.fragment_movies_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val apiKey = BuildConfig.API_KEY
+        Toast.makeText(context, "API_KEY: $apiKey", Toast.LENGTH_SHORT).show()
     }
 }
